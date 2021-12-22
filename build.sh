0224356150dcb6feba81d2e1076d6072363a2010
kergma <pushkinsv@gmail.com>
2021-12-22 19:07:03 +0700

    repo name update

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 TAG=$1
 IMAGE=$TAG
 [ "$IMAGE" = "" ] || IMAGE=":$IMAGE"
-IMAGE="listcontrols/backend$IMAGE"
+IMAGE="opendrugs/backend$IMAGE"
 
 BUILD=`git log HEAD --pretty=oneline --relative .| wc -l`
 REVISION=`git rev-parse --short HEAD`
