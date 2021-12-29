8c01b7d2dcc4aeb4186e4e0a32120a7fe90c38f2
sa.zheldokas <sa.zheldokas@gmail.com>
2021-12-29 17:00:38 +0300

    fix plot request

--- api/endpoints/experiment.py
+++ api/endpoints/experiment.py
@@ -1,9 +1,9 @@
-from typing import List
+from typing import List,Optional
 
 from fastapi import APIRouter, HTTPException
 
 from config import Language
-from handlers.experimets import search, get, plot
+from handlers.experiments import search, get, plot
 from presenters.experiment import Experiment
 
 router = APIRouter()
@@ -16,22 +16,31 @@ async def get_experiments_list(
     ls = search.handler_get_exp_list(lang, page, pageSize)
     return ls
 
-@router.get(f'/{base_route}/{{idx}}')
-async def get_experiment(
-        lang: Language = Language.en,
-        idx: int = None):
-    if idx is None:
-        raise HTTPException( status_code=404, detail='missing idx arg',)
-    exp = get.handler_get_exp(lang, idx)
-    exp["id"] = idx
-    return exp
+# @router.get(f'/{base_route}/{{idx}}')
+# async def get_experiment(
+#         lang: Language = Language.en,
+#         idx: int = None):
+#     if idx is None:
+#         raise HTTPException( status_code=404, detail='missing idx arg',)
+#     exp = get.handler_get_exp(lang, idx)
+#     exp["id"] = idx
+#     return exp
 
-@router.get(f'/{base_route}/{{idx}}/plot')
+@router.get(f'/{base_route}/plot')
 async def get_experiment_plot(
         lang: Language = Language.en,
-        idx: int = None):
-    if idx is None:
-        raise HTTPException( status_code=404, detail='missing idx arg',)
-    pl = plot.handler_plot_exp(lang, idx)
+        ids: str = None):
+    if ids is None:
+        raise HTTPException( status_code=404, detail='missing ids arg',)
+    ls = []
+    for i in ids.split(','):
+        try:
+            t = i.strip()
+            if t != '':
+                ls.append(int(t))
+        except:
+            raise HTTPException( status_code=404, detail='wrong type parameter ids',)
+
+    pl = plot.handler_plot_exp(lang, ls)
     return pl
 
